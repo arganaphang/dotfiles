980d0af916717df656994cfa775fd6418bb84a1a
arganaphangquestian <arganaphangquestian@gmail.com>
2022-04-10 20:52:04 +0700

    Add yarn, exa alias

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,2 +1,13 @@
 fenv source ~/.profile
 starship init fish | source
+
+alias y="yarn"
+alias ya="yarn add"
+alias yt="yarn test"
+alias ys="yarn start"
+alias yd="yarn dev"
+
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
