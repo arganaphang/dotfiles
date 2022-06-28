f3ab940754a75545ae5c2c66a0e99d2cbc5990d0
arganaphangquestian <arganaphangquestian@gmail.com>
2022-06-28 23:53:52 +0700

    add alias

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -10,6 +10,12 @@ alias yt="yarn test"
 alias ys="yarn start"
 alias yd="yarn dev"
 
+alias gmi="go mod init"
+alias dcu="docker-compose up -d"
+alias dcd="docker-compose down --remove-orphans"
+alias dcps="docker-compose ps"
+alias dclog="docker-compose log"
+
 if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
