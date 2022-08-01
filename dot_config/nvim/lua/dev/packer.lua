67e944e6932b1ed04c31f12053afb803691e7d08
arganaphangquestian <arganaphangquestian@gmail.com>
2022-08-01 23:59:05 +0700

    add: `coc.nvim`

--- dot_config/nvim/lua/dev/packer.lua
+++ dot_config/nvim/lua/dev/packer.lua
@@ -5,4 +5,5 @@ return require('packer').startup(function(use)
   use 'folke/tokyonight.nvim'
   use 'fatih/vim-go'
   use 'kien/ctrlp.vim'
+  use {'neoclide/coc.nvim', run = "yarn install --frozen-lockfile && yarn build"}
 end)
