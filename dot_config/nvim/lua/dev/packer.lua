fea3a4c952c63395d25930d099028fdc8be9d0f5
Argana Phangquestian <arganaphangquestian@gmail.com>
2022-12-18 02:00:33 +0700

    fix: `nvim`

--- dot_config/nvim/lua/dev/packer.lua
+++ dot_config/nvim/lua/dev/packer.lua
@@ -33,4 +33,11 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    use {
+        "startup-nvim/startup.nvim",
+        requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+        config = function()
+            require"startup".setup()
+        end
+    }
 end)
--- dot_config/nvim/lua/dev/set.lua
+++ dot_config/nvim/lua/dev/set.lua
@@ -25,8 +25,4 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
 vim.g.mapleader = " "
