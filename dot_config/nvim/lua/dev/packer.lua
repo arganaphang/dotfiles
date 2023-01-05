5f0888261aa8c8ccb061f59d148fe516f3476097
Argana Phangquestian <arganaphangquestian@gmail.com>
2023-01-05 19:49:35 +0700

    add: vim-go

--- dot_config/nvim/lua/dev/packer.lua
+++ dot_config/nvim/lua/dev/packer.lua
@@ -10,6 +10,7 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use('fatih/vim-go')
     use {
         'VonHeikemen/lsp-zero.nvim',
         requires = {
