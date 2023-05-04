38805d21fc319b75c818dcf07bfa12d9a3cdced4
Argana Phangquestian <arganaphangquestian@gmail.com>
2023-05-04 19:15:52 +0700

    chore: update ocaml

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -26,5 +26,10 @@ end
 
 # pnpm
 set -gx PNPM_HOME "/Users/arga/Library/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
 # pnpm end
+
+# opam configuration
+source /Users/arga/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
